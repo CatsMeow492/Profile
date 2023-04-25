9d7dba81cd16b77d70e4c9987efdb33f0aeea604
Taylor Mohney <taylormohneytech@gmail.com>
2023-04-25 08:44:19 -0700

    hamburger menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,25 +1,77 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import styles from './Header.module.css';
+import NextImage from 'next/image';
+import Link from 'next/link';
+import { fallDown as Menu } from 'react-burger-menu';
 
 const Header: FC = () => {
+    const logo = '/tbc.svg';
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const handleStateChange = (state) => {
+        setMenuOpen(state.isOpen);
+    };
+
+    const menuStyles = {
+        bmBurgerButton: {
+            position: 'absolute',
+            width: '36px',
+            height: '30px',
+            right: '36px',
+            top: '3.7%',
+            transform: 'translateY(-50%)',
+        },
+        bmBurgerBars: {
+          background: '#fff',
+        },
+        bmMenu: {
+          background: '#333',
+          padding: '2.5em 1.5em 0',
+        },
+        bmMenuItem: {
+          display: 'block',
+          padding: '1em',
+          color: '#fff',
+          textDecoration: 'none',
+          fontWeight: 'bold',
+          outline: 'none',
+        },
+        bmMenuWrap: {
+          top: 0,
+        },
+      };
+      
+
     return (
         <header className={styles.header}>
             <nav className={styles.navbar}>
-                <a href="#" className={styles.logo}>Your Logo</a>
-                <ul className={styles.navList}>
-                    <li className={styles.navItem}>
-                        <a href="#home" className={styles.navLink}>Home</a>
-                    </li>
-                    <li className={styles.navItem}>
-                        <a href="#about" className={styles.navLink}>About</a>
-                    </li>
-                    <li className={styles.navItem}>
-                        <a href="#services" className={styles.navLink}>Services</a>
-                    </li>
-                    <li className={styles.navItem}>
-                        <a href="#contact" className={styles.navLink}>Contact</a>
-                    </li>
-                </ul>
+                <Link href="/" className={styles.logo}>
+                    <div className={styles.logoContainer} style={{ maxWidth: 'none' }}>
+                        <NextImage src={logo} alt="Your Logo" width="250" height="100" />
+                    </div>
+                </Link>
+                <Menu
+                right
+                isOpen={menuOpen}
+                onStateChange={({ isOpen }) => setMenuOpen(isOpen)}
+                styles={menuStyles}
+                >
+                    <span className={styles.closeButton} onClick={() => setMenuOpen(false)}>
+                        &times;
+                    </span>
+                    <a href="#home" className={styles.navLink} onClick={() => setMenuOpen(false)}>
+                        Home
+                    </a>
+                    <a href="#about" className={styles.navLink} onClick={() => setMenuOpen(false)}>
+                        About
+                    </a>
+                    <a href="#services" className={styles.navLink} onClick={() => setMenuOpen(false)}>
+                        Services
+                    </a>
+                    <a href="#contact" className={styles.navLink} onClick={() => setMenuOpen(false)}>
+                        Contact
+                    </a>
+                </Menu>
             </nav>
         </header>
     );
